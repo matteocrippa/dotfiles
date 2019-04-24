85b269cadbf9322cf1a8d23b7f457810533335b5
Matteo Crippa <matteocrippa@users.noreply.github.com>
2019-04-24 15:42:32 +0200

    Update formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+# manage different disk
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo gdisk /dev/sda
+  o # clear the in memory partition table
+  y # confirm cleanup
+  n # new partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +256MB # 100 MB boot parttion
+  EF00
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  8E00 # make a partition bootable
+  w # write the partition table
+  q # and we're done
+EOF
+
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo gdisk /dev/sdb
+  o # clear the in memory partition table
+  y # confirm cleanup
+  n # new partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+    # 100 MB boot parttion
+  8E00
+  w # write the partition table
+  q # and we're done
+EOF
