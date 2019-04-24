f2816857cc45c61cf5bcb177273ba2364ad2b8c2
Matteo Crippa <matteocrippa@users.noreply.github.com>
2019-04-24 09:20:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@
 . packages.sh
 . helpers.sh
 
+echo_info "Installing lenovo packages..."
+_install lenovo
+
 # Install packages in the official repositories
-# echo_info "Installing core packages..."
-# _install core
+echo_info "Installing core packages..."
+_install core
 
 # Install packages in the AUR
 # echo_info "Installing aur packages..."
