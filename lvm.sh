b95ea95c07fddb8e169ec4ca277cf3519a5e2686
Matteo Crippa <matteocrippa@users.noreply.github.com>
2019-04-24 17:29:35 +0200

    Update lvm.sh

--- lvm.sh
+++ lvm.sh
@@ -4,15 +4,17 @@
 
 echo "LVM"
 
-sudo cryptsetup luksFormat /dev/sda2
-sudo cryptsetup open --type luks /dev/sda2 foo
-sudo pvcreate /dev/mapper/foo
-sudo vgcreate foo_group /dev/mapper/foo
-sudo lvcreate -L32G  foo_group -n swap
-sudo lvcreate -l 100%FREE foo_group -n root
+# NvMe
+cryptsetup luksFormat /dev/sda2
+cryptsetup open --type luks /dev/sda2 foo
+pvcreate /dev/mapper/foo
+vgcreate foo_group /dev/mapper/foo
+lvcreate -L32G  foo_group -n swap
+lvcreate -l 100%FREE foo_group -n root
 
-sudo cryptsetup luksFormat /dev/sdb1
-sudo cryptsetup open --type luks /dev/sdb1 bar
-sudo pvcreate /dev/mapper/bar
-sudo vgcreate bar_group /dev/mapper/bar
-sudo lvcreate -l 100%FREE bar_group -n home
+# SSD
+cryptsetup luksFormat /dev/sdb1
+cryptsetup open --type luks /dev/sdb1 bar
+pvcreate /dev/mapper/bar
+vgcreate bar_group /dev/mapper/bar
+lvcreate -l 100%FREE bar_group -n home
