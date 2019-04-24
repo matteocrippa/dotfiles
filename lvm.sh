f7ad06b2ae379784d749930d9f5c3326700b0fa4
Matteo Crippa <matteo@boostco.de>
2019-04-24 22:23:03 +0200

    better naming

--- lvm.sh
+++ lvm.sh
@@ -7,22 +7,22 @@
 echo_info "LVM + Formatting NVMe"
 cryptsetup luksFormat ${NVME}2
 cryptsetup open --type luks ${NVME}2 foo
-pvcreate /dev/mapper/foo
-vgcreate foo_group /dev/mapper/foo
-lvcreate -L${SWAP}  foo_group -n swap
-lvcreate -l 100%FREE foo_group -n root
+pvcreate /dev/mapper/nvme
+vgcreate nvme_group /dev/mapper/nvme
+lvcreate -L${SWAP}  nvme_group -n swap
+lvcreate -l 100%FREE nvme_group -n root
 mkfs.fat ${NVME}1
-mkfs.ext4 /dev/mapper/foo_group-root
-mkswap /dev/mapper/foo_group-swap
+mkfs.ext4 /dev/mapper/nvme_group-root
+mkswap /dev/mapper/nvme_group-swap
 
 # SSD
 echo_info "LVM + Formatting SSD"
 cryptsetup luksFormat ${SSD}1
 cryptsetup open --type luks ${SSD}1 bar
-pvcreate /dev/mapper/bar
-vgcreate bar_group /dev/mapper/bar
-lvcreate -l 100%FREE bar_group -n home
-mkfs.ext4 /dev/mapper/bar_group-home
+pvcreate /dev/mapper/ssd
+vgcreate ssd_group /dev/mapper/ssd
+lvcreate -l 100%FREE ssd_group -n home
+mkfs.ext4 /dev/mapper/ssd_group-home
 F
 echo_info "Mounting"
 
