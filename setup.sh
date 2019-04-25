62123b92457bfff1ebbfeffc4b6a564b1fe3200c
Matteo Crippa <matteo@boostco.de>
2019-04-25 09:27:15 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -99,7 +99,14 @@ rm /etc/localtime
 ln -s /usr/share/zoneinfo/Europe/Rome /etc/localtime
 hwclock --systohc --utc
 echo "arch" > /etc/hostname
+
+read -p "Press any key to continue... " -n1 -s
+echo_info "Setup Root user"
+
 passwd
+
+echo_info "Setup pacman"
+
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 echo "Server = https://www.archlinux.org/mirrorlist/" >> /etc/pacman.d/mirrorlist
@@ -110,6 +117,9 @@ echo_info "Bootloader"
 
 sed -i "s/HOOKS/#HOOKS/g" /etc/mkinitcpio.conf
 echo "HOOKS=(base udev autodetect keyboard keymap modconf block encrypt lvm2 filesystems fsck)" >> /etc/mkinitcpio.conf
+
+read -p "Press any key to continue... " -n1 -s
+
 mkinitcpio -p linux
 bootctl --path=/boot install
 echo "default arch" > /boot/loader/loader.conf
