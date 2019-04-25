0690a6841293a8b3fb77c2640bf0c9e5b004abd6
Matteo Crippa <matteo@boostco.de>
2019-04-25 08:53:25 +0200

    fix script setup

--- setup.sh
+++ setup.sh
@@ -112,14 +112,13 @@ sed -i "s/HOOKS/#HOOKS/g" /etc/mkinitcpio.conf
 echo "HOOKS=(base udev autodetect keyboard keymap modconf block encrypt lvm2 filesystems fsck)" >> /etc/mkinitcpio.conf
 mkinitcpio -p linux
 bootctl --path=/boot install
-vi /boot/loader/loader.conf
 echo "default arch" > /boot/loader/loader.conf
 echo "editor 0" >> /boot/loader/loader.con
 echo "title Arch Linux" > /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-PART_ID=$(blkid -o value -s UUID /dev/mapper/nvme_group-root)
+export PART_ID=$(blkid -o value -s UUID /dev/mapper/nvme_group-root)
 echo "options cryptdevice=UUID=${PART_ID}:cryptlvm root=/dev/mapper/nvme_group-root quiet rw" >> /boot/loader/entries/arch.conf
 
 echo_info "Prepare reboot"
