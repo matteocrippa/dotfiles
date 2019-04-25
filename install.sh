7fa5097ae0ab728537688265d55d2800e95fc63a
Matteo Crippa <matteo@boostco.de>
2019-04-25 18:33:05 +0200

    restore

--- install.sh
+++ install.sh
@@ -12,18 +12,18 @@ git clone https://aur.archlinux.org/yay.git /tmp/yay
 cd /tmp/yay
 makepkg -si
 
-#echo_info "Installing lenovo packages..."
-#_install lenovo
-#sudo systemctl enable lenovo_fix
-#sudo systemctl start lenovo_fix
-#sudo systemctl enable fstrim.timer
-#sudo systemctl enable lenovo_fix
-#
-## Install packages in the official repositories
-#echo_info "Installing core packages..."
-#_install core
-#
-#_symlink
-#
-#echo_info "Force update all the system"
-#sudo pacman -Syyu
+echo_info "Installing lenovo packages..."
+_install lenovo
+sudo systemctl enable lenovo_fix
+sudo systemctl start lenovo_fix
+sudo systemctl enable fstrim.timer
+sudo systemctl enable lenovo_fix
+
+# Install packages in the official repositories
+echo_info "Installing core packages..."
+_install core
+
+_symlink
+
+echo_info "Force update all the system"
+sudo pacman -Syyu
