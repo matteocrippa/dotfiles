13318f7dd42ad5daee6667e86a8240e53a509017
Matteo Crippa <matteo@boostco.de>
2019-04-27 00:05:39 +0200

    fix

--- chroot.sh
+++ chroot.sh
@@ -11,7 +11,7 @@ touch /boot/loader/entries/manjaro.conf
 echo "title Arch Linux" >> /boot/loader/entries/manjaro.conf
 echo "linux /vmlinuz-5.1-x86_64" >> /boot/loader/entries/manjaro.conf
 echo "initrd /intel-ucode.img" >> /boot/loader/entries/manjaro.conf
-echo "initrd /initramfs5.1-x86_64.img" >> /boot/loader/entries/manjaro.conf
+echo "initrd /initramfs-5.1-x86_64.img" >> /boot/loader/entries/manjaro.conf
 export PART_ID=$(blkid -o value -s UUID ${NVME}2)
 echo "options cryptdevice=UUID=${PART_ID}:nvme_group root=/dev/mapper/nvme_group-root quiet rw" >> /boot/loader/entries/manjaro.conf
 
