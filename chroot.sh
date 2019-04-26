79812d630cb5e0bf2ae0a3211789d16b056a263c
Matteo Crippa <matteo@boostco.de>
2019-04-26 18:51:11 +0200

    updated chroot

--- chroot.sh
+++ chroot.sh
@@ -8,12 +8,10 @@ export PART_ID=$(blkid -o value -s UUID ${SSD}1)
 sudo echo "ssd UUID=${PART_ID} /root/keyfile luks" >> /etc/crypttab
 
 echo_info "Bootloader"
-echo "default arch" >> /boot/loader/loader.conf
-echo "editor 0" >> /boot/loader/loader.conf
 echo "title Arch Linux" > /boot/loader/entries/arch.conf
-echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "linux /vmlinuz-5.1-x86_64" >> /boot/loader/entries/arch.conf
 echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
-echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+echo "initrd /initramfs5.1-x86_64.img" >> /boot/loader/entries/arch.conf
 export PART_ID=$(blkid -o value -s UUID ${NVME}2)
 echo "options cryptdevice=UUID=${PART_ID}:nvme_group root=/dev/mapper/nvme_group-root quiet rw" >> /boot/loader/entries/arch.conf
 
