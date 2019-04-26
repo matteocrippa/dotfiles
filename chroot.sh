084a9cc088002636267cdc8052de688b4ca86ebe
Matteo Crippa <matteo@boostco.de>
2019-04-26 18:46:26 +0200

    fix

--- chroot.sh
+++ chroot.sh
@@ -6,18 +6,18 @@
 echo_info "Prepare keyfile"
 export PART_ID=$(blkid -o value -s UUID ${SSD}1)
 sudo echo "ssd UUID=${PART_ID} /root/keyfile luks" >> /etc/crypttab
-#
-#echo_info "Bootloader"
-#mkdir /boot/loader
-#mkdir /boot/loader/entries
-#echo "default arch" >> /boot/loader/loader.conf
-#echo "editor 0" >> /boot/loader/loader.conf
-#echo "title Arch Linux" > /boot/loader/entries/arch.conf
-#echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
-#echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
-#echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-#export PART_ID=$(blkid -o value -s UUID ${NVME}2)
-#echo "options cryptdevice=UUID=${PART_ID}:nvme_group root=/dev/mapper/nvme_group-root quiet rw" >> /boot/loader/entries/arch.conf
 
-echo_info "Prepare reboot"
+echo_info "Bootloader"
+mkdir /boot/loader
+mkdir /boot/loader/entries
+echo "default arch" > /boot/loader/loader.conf
+echo "editor 0" >> /boot/loader/loader.conf
+echo "title Arch Linux" > /boot/loader/entries/arch.conf
+echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
+echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+export PART_ID=$(blkid -o value -s UUID ${NVME}2)
+echo "options cryptdevice=UUID=${PART_ID}:nvme_group root=/dev/mapper/nvme_group-root quiet rw" >> /boot/loader/entries/arch.conf
+
+echo_info "Edit /boot/loader/entries/arch.conf according proper files"
 exit
