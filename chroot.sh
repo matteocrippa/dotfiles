dcd236a395710e69983b4ef04738725bd8b62ec1
Matteo Crippa <matteo@boostco.de>
2019-04-27 00:03:15 +0200

    fix

--- chroot.sh
+++ chroot.sh
@@ -7,17 +7,13 @@ echo_info "Prepare keyfile"
 export PART_ID=$(blkid -o value -s UUID ${SSD}1)
 sudo echo "ssd UUID=${PART_ID} /root/keyfile luks" >> /etc/crypttab
 
-echo_info "Bootloader"
-mkinitcpio -p linux
-bootctl --path=/boot install
-
-#touch /boot/loader/entries/arch.conf
-#echo "title Arch Linux" >> /boot/loader/entries/arch.conf
-#echo "linux /vmlinuz-5.1-x86_64" >> /boot/loader/entries/arch.conf
-#echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
-#echo "initrd /initramfs5.1-x86_64.img" >> /boot/loader/entries/arch.conf
-#export PART_ID=$(blkid -o value -s UUID ${NVME}2)
-#echo "options cryptdevice=UUID=${PART_ID}:nvme_group root=/dev/mapper/nvme_group-root quiet rw" >> /boot/loader/entries/arch.conf
+touch /boot/loader/entries/manjaro.conf
+echo "title Arch Linux" >> /boot/loader/entries/manjaro.conf
+echo "linux /vmlinuz-5.1-x86_64" >> /boot/loader/entries/manjaro.conf
+echo "initrd /intel-ucode.img" >> /boot/loader/entries/manjaro.conf
+echo "initrd /initramfs5.1-x86_64.img" >> /boot/loader/entries/manjaro.conf
+export PART_ID=$(blkid -o value -s UUID ${NVME}2)
+echo "options cryptdevice=UUID=${PART_ID}:nvme_group root=/dev/mapper/nvme_group-root quiet rw" >> /boot/loader/entries/manjaro.conf
 
 echo_info "Edit /boot/loader/entries/arch.conf according proper files"
 exit
