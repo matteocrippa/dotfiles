fc70e6081445048669c80a2e2039cd228e16a1e6
Matteo Crippa <matteo@boostco.de>
2019-05-01 14:17:23 +0200

    updated zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,19 +5,13 @@
 # shellcheck source=helpers.sh
 . ../helpers.sh
 
-echo_info "Installing ZSH with OH-MY-ZSH..."
-_install zsh
+echo_info "Installing zsh-autosuggestions, zsh-completions..."
+_install zsh-completions
+_install zsh-autosuggestions
 
-echo_info "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo_info "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-
-echo_info "Symlink .zshrc..."
-ln -sfT "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
-
-echo_info "Changing shell..."
-chsh -s "$(command -v zsh)"
+echo_info "Installing Spaceship"
+git clone https://aur.archlinux.org/spaceship-prompt-git.git /tmp/spaceship-prompt-git
+cd /tmp/spaceship-prompt-git
+makepkg -si
 
 echo_done "ZSH configuration!"
