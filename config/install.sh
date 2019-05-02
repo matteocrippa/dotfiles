df74cc28d7bb8dc555a33f48094456487dc8a4b5
Matteo Crippa <matteo@boostco.de>
2019-05-02 08:27:21 +0200

    updated fonts ;

--- config/install.sh
+++ config/install.sh
@@ -12,17 +12,7 @@ echo_info "Symlink libinput..."
 ls -sfT ~/.dotfiles/config/libinput-getstures.conf ~/.config/libinput-gestures.conf
 
 echo_info "Downloading fonts..."
-mkdir -p ~/.fonts ~/Downloads/fonts
-
-curl https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip \
-  -o ~/Downloads/fonts/emoji-font.zip
-
-wget -O ~/Downloads/fonts/Hack.zip \
-  https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hack.zip
-
 wget -O ~/Downloads/fonts/fontawesome.zip \
   https://use.fontawesome.com/releases/v5.7.1/fontawesome-free-5.7.1-desktop.zip
-
-sudo cp ~/.dotfiles/config/50-noto-color-emoji.conf /etc/fonts/conf.d/
 
 yay -Sy nerd-fonts-complete
