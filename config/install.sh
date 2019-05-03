b8d3cd1ff217ccb8b9c8c1dc9798c5c6df2ba264
Matteo Crippa <matteo@boostco.de>
2019-05-03 22:20:42 +0200

    fix lib gestures

--- config/install.sh
+++ config/install.sh
@@ -11,7 +11,7 @@ ln -sfT ~/.dotfiles/config/profile ~/.profile
 echo_info "Enable gestures..."
 yay -Sy libinput-gestures
 sudo gpasswd -a $USER input
-ln -sfT ~/.dotfiles/config/libinput-getstures.conf ~/.config/libinput-gestures.conf
+ln -sfT ~/.dotfiles/config/libinput-gestures.conf ~/.config/libinput-gestures.conf
 
 yay -Sy nerd-fonts-complete
 yay -Sy ttf-font-awesome
