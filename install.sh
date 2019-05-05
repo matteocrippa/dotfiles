c9b13762a32cc57ac631687dc0315c977b07fd44
Matteo Crippa <matteocrippa@users.noreply.github.com>
2019-05-05 16:05:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 . packages.sh
 . helpers.sh
 
+echo_info "Force update all the system"
+yay -Syyu
+
 echo_info "Install Yay"
 sudo pacman -Sy yay
 
@@ -23,11 +26,15 @@ echo_info "Active remote ntp time"
 sudo systemctl enable systemd-timesyncd
 sudo systemctl start systemd-timesyncd
 
+echo_info "Activate libvirtual"
+sudo systemctl enable libvirtd
+sudo systemctl start libvirtd
+
 # Install displaylink
-echo_info "Installing displaylink..."
-ysy -Sy evdi linux-rt-lts-headers displaylink
-sudo systemctl enable displaylink.service
-sudo cp ./displaylink/20-evdidevice.conf /usr/share/X11/xorg.conf.d/20-evdidevice.conf
+#echo_info "Installing displaylink..."
+#yay -Sy evdi linux-rt-lts-headers displaylink
+#sudo systemctl enable displaylink.service
+#sudo cp ./displaylink/20-evdidevice.conf /usr/share/X11/xorg.conf.d/20-evdidevice.conf
 
 # Setup extra keys
 sudo echo "evdev:name:ThinkPad Extra Buttons:dmi:bvn*:bvr*:bd*:svnLENOVO*:pn*" > /etc/udev/hwdb.d/90-thinkpad-keyboard.hwdb
@@ -40,9 +47,6 @@ sudo udevadm trigger --sysname-match="event*"
 # Install packages in the official repositories
 echo_info "Installing core packages..."
 _install core
-
-echo_info "Force update all the system"
-yay -Syyu
 
 echo_info "Force remove unused apps"
 yay -Rs palemoon-bin
@@ -60,13 +64,7 @@ systemctl --user enable grive-timer@$(systemd-escape ${GDRIVE_DIR}).timer
 systemctl --user start grive-timer@$(systemd-escape ${GDRIVE_DIR}).timer
 systemctl --user enable grive-changes@$(systemd-escape ${GDRIVE_DIR}).service
 systemctl --user start grive-changes@$(systemd-escape ${GDRIVE_DIR}).service
+cd ~
 
-echo_info "Setup scanner"
-sudo brsaneconfig4 -a name="Brother" model="DCP1610W" ip=192.168.0.16
-
-echo_info "Setup MacOS VM"
-yay -Sy dmg2img
-cd /tmp
-git clone https://github.com/matteocrippa/macos-guest-virtualbox.git
-cd macos-guest-virtualbo
-./macos-guest-virtualbox.sh
+#echo_info "Setup scanner"
+#sudo brsaneconfig4 -a name="Brother" model="DCP1610W" ip=192.168.0.16
