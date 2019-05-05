5d48c0c648de569a45acc78131d949371eb4c8c5
Matteo Crippa <matteo.crippa+dev@gmail.com>
2019-05-06 00:22:05 +0200

    improved macos script

--- macos.sh
+++ macos.sh
@@ -2,9 +2,10 @@
 yay -Sy base-devel qemu uml_utilities virt-manager dmg2img ebtables dnsmasq
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
+mkdir -p ~/Vm
 cd ~
-git clone git://github.com/kholia/OSX-KVM
-cd OSX-KVM
+git clone git://github.com/kholia/OSX-KVM ~/Vm/Osx
+cd ~/Vm/Osx
 ./fetch-macOS.py
 dmg2img BaseSystem.dmg BaseSystem.img
 qemu-img create -f qcow2 mac_hdd_ng.img 64G
