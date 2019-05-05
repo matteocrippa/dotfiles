75c04e4b0026832cda34b6774af7f5f6dc5faf98
Matteo Crippa <matteo.crippa+dev@gmail.com>
2019-05-05 23:28:31 +0200

    cleanup dotfiles

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,18 +5,6 @@
 # shellcheck source=helpers.sh
 . ../helpers.sh
 
-echo_info "Installing zsh-autosuggestions, zsh-completions..."
-_install zsh-completions
-_install zsh-autosuggestions
-
-echo_info "Installing Spaceship"
-git clone https://aur.archlinux.org/spaceship-prompt-git.git /tmp/spaceship-prompt-git
-cd /tmp/spaceship-prompt-git
-makepkg -si
-
-echo "autoload -U promptinit; promptinit" >> ~/.zshrc
-echo "prompt spaceship" >> ~/.zshrc
-
 echo "alias vi=nvim" >> ~/.zshrc
 
 echo_done "ZSH configuration!"
