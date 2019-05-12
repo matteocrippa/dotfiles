225c2806d9c44e461a1110d3c21d634b389667e5
Matteo Crippa <matteo.crippa+dev@gmail.com>
2019-05-12 21:57:04 +0200

    improved rofi aliases

--- rofi/install.sh
+++ rofi/install.sh
@@ -11,5 +11,6 @@ _install rofi
 echo_info "Symlink rofi config"
 mkdir -p ~/.config/rofi
 ln -sfT ~/.dotfiles/rofi/config ~/.config/rofi/config
+ln -sfT ~/.dotfiles/rofi/aliases.sh ~/.config/rofi/aliases.sh
 
 echo_done "Rofi configuration!"
