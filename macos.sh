b2a228e0e2940993bd51a5746b8291a04a52f263
Matteo Crippa <matteo.crippa+dev@gmail.com>
2019-05-12 16:15:29 +0200

    improved macos

--- macos.sh
+++ macos.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-git clone git@github.com:foxlet/macOS-Simple-KVM.git ~/Vm/macOS
+git clone git@github.com:matteocrippa/macOS-Simple-KVM.git ~/Vm/macOS
 cd ~/Vm/macOS
+./install.sh
 ./jumpstart.sh
 qemu-img create -f qcow2 MyDisk.qcow2 64G
-echo "-drive id=SystemDisk,if=none,file=MyDisk.qcow2 \\" >> basic.sh
-echo "-device ide-hd,bus=sata.4,drive=SystemDisk \\" >> basic.sh
-./basic.sh
-./make.sh --add
